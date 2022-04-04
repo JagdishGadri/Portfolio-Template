bdba56068ee1f6c2b42c940fc510f7e666b8ef05
Jagdish <jagdish.g@simformsolutions.com>
2022-04-04 14:36:34 +0530

    changed styles of cards

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -20,7 +20,7 @@ const Intro = () => {
           <p className="i-desc">
             I as a Business analyst help guide businesses in improving processes,
             products, services and software through data analysis. I am a agile
-            worker who straddl// const theme = useContext(ThemeContext);e the line between IT and the business to help bridge
+            worker who straddle the line between IT and the business to help bridge
             the gap and improve efficiency
           </p>
         </div>
